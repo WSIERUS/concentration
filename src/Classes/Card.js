fc3d03a693300b0bad27c01986a02eb9a961a309
Wojciech <wojt.sier@o2.pl>
2022-06-12 10:55:48 +0200

    Card Class

--- src/Classes/Card.js
+++ src/Classes/Card.js
@@ -4,6 +4,28 @@
 // - odkryta 
 // - nie odkryta
 
+let defaultImage = './Images/default.jpg'
+
 class Card {
-  
+
+  constructor(id, image, classe, onClick, classeChanged, clicked) {
+    this.id = id,
+    this.image = image,
+    this.classe = classe,
+    this.onClick = onClick,
+    this.classeChanged = classeChanged
+    this.clicked = clicked
+    this.handleRender = this.handleCreate
+  }
+
+  handleCreate = () => {
+    const element = document.createElement('div')
+    element.style.backgroundImage = `url(${defaultImage})`
+    element.setAttribute('alt', 'image')
+    element.className = classe
+    element.addEventListener('click', () => this.onClick(this.id, this.image, this.clicked))
+    element.addEventListener('mouseenter', element.className = this.classeChanged)
+    element.addEventListener('mouseleave', element.className = this.classe)
+  }
+
 }
